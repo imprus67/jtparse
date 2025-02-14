be29004ac425d8d72ace0536fb0205d9b13c4678
konst <my@email.com>
2025-02-14 03:34:54 +0300

    another commit

--- handlers.ts
+++ handlers.ts
@@ -1,12 +1,85 @@
+import { messageTypes } from './types/messagetypes';
 import { PrismaClient } from '@prisma/client';
+import fs from 'fs';
+import random from 'random';
+import { Api, TelegramClient } from 'telegram';
 import { NewMessageEvent } from 'telegram/events';
+import { v4 as uuidv4 } from 'uuid';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+export async function sendMessageCustom(client, peer: string, message: string) {
+	await client.connect(); // This assumes you have already authenticated with .start()
 
-export async function saveTextMessage(event: NewMessageEvent) {
-  await prisma.textMessage.create({
-    data: {
-      messageText: event.message.message,
-    },
-  });
+	const result: Api.Updates = await client.invoke(
+		new Api.messages.SendMessage({
+			peer,
+			message,
+			randomId: random.int(1, 9999999999999),
+			noWebpage: true,
+			noforwards: true,
+			scheduleDate: 43,
+			sendAs: peer,
+		})
+	);
+	console.log(result); // prints the result
+}
+
+export async function downloadPhoto(event, client: TelegramClient) {
+	const photo = event.message.photo;
+	const buffer = await client.downloadFile(
+		new Api.InputPhotoFileLocation({
+			id: photo.id,
+			accessHash: photo.accessHash,
+			fileReference: photo.fileReference,
+			thumbSize: 'y',
+		}),
+		{
+			dcId: photo.dcId,
+			//fileSize: "y",
+		}
+	);
+
+	fs.writeFileSync(uuidv4() + '.jpg', buffer);
+}
+
+export async function saveTextMessage(
+	event: NewMessageEvent,
+	prisma: PrismaClient
+) {
+	await prisma.message
+		.create({
+			data: {
+				messageType: messageTypes.messageText,
+				messageText: event.message.message,
+				messageMedia: false,
+				fwdFrom: JSON.stringify(event.message.fwdFrom) || null,
+			},
+		})
+		.catch(e => {
+			throw e;
+		})
+		.finally(async () => {
+			await prisma.$disconnect();
+		});
+}
+
+export async function savePhotoMessage(
+	event: NewMessageEvent,
+	prisma: PrismaClient
+) {
+	await prisma.message
+		.create({
+			data: {
+				messageType: messageTypes.messagePhoto,
+				messageText: event.message.message || 'noText',
+				messageMedia: true,
+				fwdFrom: JSON.stringify(event.message.fwdFrom) || null,
+			},
+		})
+		.catch(e => {
+			throw e;
+		})
+		.finally(async () => {
+			await prisma.$disconnect();
+		});
 }
--- index.ts
+++ index.ts
@@ -1,88 +1,80 @@
-import { saveTextMessage } from './handlers';
+import {
+	downloadPhoto,
+	savePhotoMessage,
+	saveTextMessage,
+	sendMessageCustom,
+} from './handlers';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 import input from 'input';
 import { TelegramClient } from 'telegram';
 import { NewMessage, NewMessageEvent } from 'telegram/events/index.js';
 import { StringSession } from 'telegram/sessions/index.js';
+import { getInnerText } from 'telegram/Utils';
 
 const prisma = new PrismaClient();
 const apiId = +process.env.API_ID;
 const apiHash = process.env.API_HASH;
 const stringSession = new StringSession(process.env.SESSION_ID); // fill this later with the value from session.save()
 (async () => {
-  console.log('Loading interactive example...');
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-  });
-  await client.start({
-    phoneNumber: async () => await input.text('Please enter your number: '),
-    password: async () => await input.text('Please enter your password: '),
-    phoneCode: async () =>
-      await input.text('Please enter the code you received: '),
-    onError: err => console.log(err),
-  });
-  console.log('You should now be connected.');
-  // await client.sendMessage("me", { message: "Hello!" });
+	console.log('Loading interactive example...');
+	const client = new TelegramClient(stringSession, apiId, apiHash, {
+		connectionRetries: 5,
+	});
+	await client.start({
+		phoneNumber: async () => await input.text('Please enter your number: '),
+		password: async () => await input.text('Please enter your password: '),
+		phoneCode: async () =>
+			await input.text('Please enter the code you received: '),
+		onError: err => console.log(err),
+	});
+	console.log('You should now be connected.');
 
-  async function handler(event: NewMessageEvent) {
-    if (event.message.media === null || event.message.media === undefined) {
-      //MessageMediaEmpty
-      saveTextMessage(event)
-        .catch(e => {
-          throw e;
-        })
-        .finally(async () => {
-          await prisma.$disconnect();
-        });
-    } else {
-      let messageType = event.message.media.className;
+	async function handler(event: NewMessageEvent) {
+		if (event.message.media === null || event.message.media === undefined) {
+			//MessageMediaEmpty
+			saveTextMessage(event, prisma);
+			// sendMessageCustom(client, process.env.CHANNEL_FOR_SENDING_ID, event.message.message);
+		} else {
+			let messageType = event.message.media.className;
 
-      switch (messageType) {
-        case 'MessageMediaPhoto':
-          console.log('MessageMediaPhoto');
-          break;
-        case 'MessageMediaDocument':
-          console.log('MessageMediaDocument');
-          break;
-        case 'MessageMediaWebPage':
-          console.log('MessageMediaWebPage');
-          break;
-        case 'MessageMediaContact':
-          console.log('MessageMediaContact');
-          break;
-        case 'MessageMediaVenue':
-          console.log('MessageMediaVenue');
-          break;
-        case 'MessageMediaGeo':
-          console.log('MessageMediaGeo');
-          break;
-        case 'MessageMediaInvoice':
-          console.log('MessageMediaInvoice');
-          break;
-        case 'MessageMediaPoll':
-          console.log('MessageMediaPoll');
-          break;
-        case 'MessageMediaStory':
-          console.log('MessageMediaStory');
-          break;
-        case 'MessageMediaDice':
-          console.log('MessageMediaDice');
-          break;
-        default:
-          console.log(event);
-      }
-    }
-    /* else if (event.message.media.className === "MessageMediaPhoto") {
-      console.log("Message - MediaPhoto");
-    } else if (event.message.media.className === "MessageMediaDocument") {
-      console.log("Message - Document");
-    } else if (event.message.media.className === "MessageMediaWebPage") {
-      console.log("Message - WebPage");
-    } else {
-      console.log(event);
-    } */
-  }
+			switch (messageType) {
+				case 'MessageMediaPhoto':
+					savePhotoMessage(event, prisma);
+					downloadPhoto(event, client);
+					break;
+				case 'MessageMediaDocument':
+					console.log('MessageMediaDocument');
+					break;
+				case 'MessageMediaWebPage':
+					console.log('MessageMediaWebPage');
+					break;
+				case 'MessageMediaContact':
+					console.log('MessageMediaContact');
+					break;
+				case 'MessageMediaVenue':
+					console.log('MessageMediaVenue');
+					break;
+				case 'MessageMediaGeo':
+					console.log('MessageMediaGeo');
+					break;
+				case 'MessageMediaInvoice':
+					console.log('MessageMediaInvoice');
+					break;
+				case 'MessageMediaPoll':
+					console.log('MessageMediaPoll');
+					break;
+				case 'MessageMediaStory':
+					console.log('MessageMediaStory');
+					break;
+				case 'MessageMediaDice':
+					console.log('MessageMediaDice');
+					break;
+				default:
+					console.log(event);
+			}
+		}
+	}
 
-  client.addEventHandler(handler, new NewMessage({}));
+	client.addEventHandler(handler, new NewMessage({}));
 })();
